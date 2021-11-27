b8dd72e9e5850c8c566459418fe4ffccbfa22eb7
wecreate <2633132826@qq.com>
2021-11-27 22:31:13 +0800

    提交 Node.js 学习记录

--- 04-Node/express-demo/app.js
+++ 04-Node/express-demo/app.js
@@ -3,12 +3,13 @@ var express = require('express');
 // 1、创建 app
 var app = express();
 
-app.get('/', function (req, res) {
-  //   res.write('hello ');
-  //   res.write('world ');
-  //   res.end();
-  //   res.end('hello world');
+// 当以 /public/ 开头时，就去 ./public/ 目录中查找对应的资源
+// app.use('/public/', express.static('./public/'));
 
+// 当省略第一个参数时，则可以通过省略 /public 的方式访问
+app.use(express.static('./public/'));
+
+app.get('/', function (req, res) {
   res.send('hello world');
 });
 
